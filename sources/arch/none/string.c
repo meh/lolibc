8263fe97fdcee727ef3cc60eaa584c7a8328da93
meh <meh.ffff@gmail.com>
2010-02-12 23:10:52 +0100

    I'm a faggot.

--- sources/arch/none/string.c
+++ sources/arch/none/string.c
@@ -96,7 +96,7 @@ __memset (void* s, int c, size_t n)
     size_t i;
 
     for (i = 0; i < n; i++) {
-        ((char*) dest)[i] = (char) c;
+        ((char*) s)[i] = (char) c;
     }
 
     return s;
